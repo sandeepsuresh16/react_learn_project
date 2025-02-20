d32e60aff7c9dd8b3c9aa3ad9e12f33413fa343a
Sandeep Sureshkumar <sandeeps@ssdiblr.sharp.co.in>
2025-02-20 19:21:11 +0530

    todo - functionality

--- todo-app/src/components/TodoItem.jsx
+++ todo-app/src/components/TodoItem.jsx
@@ -3,10 +3,20 @@ export default function TodoItem({item, todos, setTodos}) {
     function handleDelete(item) {
         setTodos(todos.filter((todo) => todo.name !== item.name))
     }
+    function handleClick(name) {
+        const newList = todos.map((todo) => todo.name === name ? {...todo,done: !todo.done} : todo)
+        setTodos(newList)
+    }
+    const newClass = item.done ? styles.comoleted : ""
     return (
         <div className={styles.item}>
             <div className={styles.itemname}>
-                {item.name}
+                <span 
+                    className={newClass} 
+                    onClick={() => handleClick(item.name)}
+                >
+                    {item.name}
+                </span>
                 <span>
                     <button onClick={()=>handleDelete(item)} className={styles.deletebutton}>x</button>
                 </span>
